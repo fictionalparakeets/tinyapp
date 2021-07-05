8066b4b2a444730362051686b0eed237f3a3279c
fictionalparakeets <banksmatthew32@gmail.com>
2021-07-05 18:33:20 +0000

    update server page to render urls from object

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -8,7 +9,23 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render('pages/index');
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get("/hello", (req, res) => {
+
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/about", (req, res) => {
+  res.render('pages/about');
+
+  // res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
